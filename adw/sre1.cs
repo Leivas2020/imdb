e057e19ce4b78d5677ef76f3c6564f71ce22f017
User <User@LAB22351>
2023-03-06 14:01:39 +0200

    Add project files.

---
+++ adw/sre1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace adw
+{
+  class sre1
+  {
+    public string name;
+    public string surmane;
+    public string email;
+    public int age;
+    
+  }
+}
